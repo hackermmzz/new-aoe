f676a94f241942c46831a4b32d39b060cab54584
胡一楠 <918677924@qq.com>
2024-07-13 03:39:27 +0800

    图片资源更新

--- enemyai.cpp
+++ enemyai.cpp
@@ -43,7 +43,7 @@ void EnemyAI::processData(){
         else if(mode==3){
         for(int i=0;i<enemyInfo.enemy_buildings_n&&find!=1;i++){
             buildtype=enemyInfo.enemy_buildings[i].Type;
-            if(buildtype==BUILDING_CENTER)
+            if(buildtype==BUILDING_ARROWTOWER)
                { targetBuilding=i;
                  find=1;
                 }
