6ff73e9a894234f74c1ce873ed34a9e5ec529ad9
yvxin <wangyv_x@163.com>
2025-03-05 09:38:29 +0000

    删除文件 open-qt-designer.bat

--- open-qt-designer.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM 设置Qt环境变量
-set PATH=C:\ProgramData\Qt5.9.2\5.9.2\mingw53_32\bin;C:\ProgramData\Qt5.9.2\Tools\mingw530_32\bin;%PATH%
-set QTDIR=C:\ProgramData\Qt5.9.2\5.9.2\mingw53_32
-
-echo 正在启动Qt Designer...
-start "" "C:\ProgramData\Qt5.9.2\5.9.2\mingw53_32\bin\designer.exe"
-
-echo 已启动Qt Designer 
