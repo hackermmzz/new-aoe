c477f45061a599b991e9c8cf983ea26f286f95a1
Ruheluobixieqing <2244216991@qq.com>
2025-03-07 14:36:50 +0800

    关闭声音

--- config.h
+++ config.h
@@ -81,8 +81,8 @@
 #define KEY_RIGHT 68
 #define BLOCK_COUNT 40     // Block种类计数，包括所有种类和样式的地图块数量
 #define FRAMES_PER_SECOND 25    //每秒帧数
-#define OPTION_MUSIC true
-#define OPTION_SOUND true
+#define OPTION_MUSIC false
+#define OPTION_SOUND false
 #define OPTION_SELECT false
 #define OPTION_LINE false
 #define OPTION_POS false
