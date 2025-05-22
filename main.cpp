5e65666603a5392f66f7bd196b2fd2e66659ac0f
yvxin <wangyv_x@163.com>
2025-05-22 19:17:48 +0800

    添加日志排除文件功能，优化指令去重逻辑

--- main.cpp
+++ main.cpp
@@ -3,22 +3,26 @@
 #include <QMap>
 #include "Logger.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     QResource::registerResource("./res.rcc");
     Logger::init(Logger::LogLevel::Debug);
 
+    // 添加排除文件，这些文件不会被Logger处理
+    Logger::addExcludedFile("EnemyAI.cpp");
+    Logger::addExcludedFile("UsrAI.cpp");
+
     QStringList args = a.arguments();
     QMap<QString, int> mapOptions = {
         {"-l", 1},{"-last",1},    // 读取上一次的地图，"tmpMap.txt"
         {"-s", 2},{"-select",2}   // 读取指定地图，"gameMap.txt"
-                                // 添加更多启动参数...
+        // 添加更多启动参数...
     };
     int mapJudge = 0;  // 默认值
 
     //提取选项对应的参数
-    for (const QString &arg : args) {
+    for (const QString& arg : args) {
         if (mapOptions.contains(arg)) {
             mapJudge = mapOptions.value(arg);
         }
