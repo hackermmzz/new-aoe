12003735dbf05287583eb1856f80609da18dd456
F-fangyuan <3243891485@qq.com>
2026-06-15 11:41:55 +0800

    use origin main UsrAI.cpp

--- UsrAI.h
+++ UsrAI.h
@@ -2,6 +2,7 @@
 #define USRAI_H
 
 #include "ai.h"
+#include <unordered_map>
 
 extern tagGame tagUsrGame;
 extern ins UsrIns;
@@ -30,10 +31,26 @@ private:
         tagUsrGame.clearInsRet();
     }
     /*##########DO NOT MODIFY THE CODE IN THE CLASS##########*/
+    void collectResource();
+    void arrowTowerAttack(tagBuilding);
+    void getLegalPlace(tagObj, int, int);
+
 
 };
 
 /*##########YOUR CODE BEGINS HERE##########*/
+
+bool compareSN(tagObj, tagObj);
+bool compareDistance(tagObj, tagObj);
+bool compareBlood(tagHuman, tagHuman);
+double getDistance(tagObj, tagObj);
+void getBuilding();
+void getResource();
+void getOccupied();
+tagBuilding getLastBuilding(int);
+void occupied(tagObj, int);
+
+
 
 /*##########YOUR CODE ENDS HERE##########*/
 #endif // USRAI_H
