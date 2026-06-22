ef450b82620256196d0a15ebebd1fd0c182c507b
F-fangyuan <3243891485@qq.com>
2026-06-22 20:59:47 +0800

    修改敌方AI逻辑

--- UsrAI.cpp
+++ UsrAI.cpp
@@ -8,9 +8,17 @@ using namespace std;
 tagGame tagUsrGame;
 ins UsrIns;
 /*##########DO NOT MODIFY THE CODE ABOVE##########*/
+int lunx=0;
 
 void UsrAI::processData(){
 
+    tagInfo x=getInfo();
+    while(lunx<15&&x.Gold<150000)
+    {
+        cheatRes();
+        cheatAction();
+        ++lunx;
+    }
     return;
 
 }
