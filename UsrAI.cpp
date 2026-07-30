41010c0fb849f9cdab11641ed23524dd79489ae4
czx <2993977710@qq.com>
2026-07-30 20:18:02 +0800

    初始化恢复干净

--- UsrAI.cpp
+++ UsrAI.cpp
@@ -9,22 +9,9 @@ using namespace std;
 tagGame tagUsrGame;
 ins UsrIns;
 /*##########DO NOT MODIFY THE CODE ABOVE##########*/
-int lunx=0;
-
-int logo=0;
-
-static bool towerTargetInRange(int towerDR, int towerUR, int objDR, int objUR)
-{
-    const int range = static_cast<int>(DIS_ARROWTOWER);
-    return abs(objDR - towerDR) <= range && abs(objUR - towerUR) <= range;
-}
 
 void UsrAI::processData()
 {
 
-    while(logo<15){
-        cheatAction();
-        cheatRes();
-        logo++;
-    }
+    //代码请写在这里
 }
